59496d21a9c6fb33e4f4bfeacc309eb83fce89e3
Gaguine <gbutera12@gmail.com>
2024-12-26 17:35:15 +0300

    Initial commit.

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+This should be a straightforward project.
+
+1. The text should be already cleaned, yet to be parsed.
+2. create an inferance, where the LLM model analyses each phrase.
+3. Store the gained information(probably a semantic value)
+4. Plot the data to see the emotion dynamics, using matplot lib
+"""
